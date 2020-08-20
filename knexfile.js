7baf027ed0c25ebe7ead1ee91d7be07a223445dd
aland1013 <adam.land@gmail.com>
2020-08-19 23:46:44 -0400

    troubleshooting deployment

--- knexfile.js
+++ knexfile.js
@@ -23,15 +23,16 @@ module.exports = {
     client: 'pg',
     connection: pgConnection,
     pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys=ON', done);
-      },
-      migrations: {
-        directory: './data/migrations'
-      },
-      seeds: {
-        directory: './data/seeds'
-      }
+      min: 2,
+      max: 10
+      // afterCreate: (conn, done) => {
+      //   conn.run('PRAGMA foreign_keys=ON', done);
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 };

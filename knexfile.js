5103c3f33417edc4a5569f8af73c050b713c06db
aland1013 <adam.land@gmail.com>
2020-08-20 15:19:10 -0400

    fixed typo in knexfile

--- knexfile.js
+++ knexfile.js
@@ -25,13 +25,13 @@ module.exports = {
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys=ON', done);
-      },
-      migrations: {
-        directory: './data/migrations'
-      },
-      seeds: {
-        directory: './data/seeds'
       }
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 };

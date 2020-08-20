9063cf95da8c6412a26f922f8687bee67369f433
aland1013 <adam.land@gmail.com>
2020-08-20 15:31:27 -0400

    more pg stuff

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,15 +1,28 @@
 const cleaner = require('knex-cleaner');
+const environment = process.env.NODE_ENV || 'development';
 
-function cleanTables(knex) {
-  return cleaner
-    .clean(knex, {
-      mode: 'truncate',
-      restartIdentity: true,
-      ignoreTables: ['knex_migrations', 'knex_migrations_lock']
-    })
-    .then(() => console.log('\n*** Tables truncated. Ready to seed. *** \n'));
+if (environment === 'development') {
+  function cleanTables(knex) {
+    return cleaner
+      .clean(knex, {
+        mode: 'truncate',
+        restartIdentity: true,
+        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+      })
+      .then(() => console.log('\n*** Tables truncated. Ready to seed. *** \n'));
+  }
+
+  exports.seed = function (knex) {
+    return knex.raw('PRAGMA foreign_keys = OFF').then(() => cleanTables(knex));
+  };
+} else {
+  exports.seed = function (knex) {
+    return cleaner
+      .clean(knex, {
+        mode: 'truncate',
+        restartIdentity: true,
+        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+      })
+      .then(() => console.log('\n*** Tables truncated. Ready to seed. *** \n'));
+  };
 }
-
-exports.seed = function (knex) {
-  return knex.raw('PRAGMA foreign_keys = OFF').then(() => cleanTables(knex));
-};

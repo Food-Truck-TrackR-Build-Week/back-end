b21b9f5b13594d88dfcb0d1ae30ea6bfc2fc3780
aland1013 <adam.land@gmail.com>
2020-08-28 23:00:15 -0400

    finished test files for server, auth-router and diners-router

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -2,33 +2,19 @@ const request = require('supertest');
 const server = require('../api/server');
 const db = require('../data/db-config');
 
-process.env = Object.assign(process.env, {
-  JWT_SECRET:
-    'say hard times you could find it aint the way that you want but its still alright'
-});
-
 describe('auth-router.test.js', () => {
   it('should set up testing environment', () => {
     expect(process.env.DB_ENV).toBe('testing');
-    expect(process.env.JWT_SECRET).toBe(
-      'say hard times you could find it aint the way that you want but its still alright'
-    );
   });
 });
 
 describe('auth-router', () => {
   let res = {};
 
-  beforeAll(async () => {
-    await db('users').truncate();
-    await db('diners').truncate();
-    await db('operators').truncate();
-  });
-
   describe('POST /register/diner', () => {
     beforeAll(async () => {
       res = await request(server).post('/api/auth/register/diner').send({
-        username: 'diner',
+        username: 'diner3',
         password: 'password',
         email: 'diner@gmail.com'
       });
@@ -40,6 +26,37 @@ describe('auth-router', () => {
 
     it('should return 201', () => {
       expect(res.status).toBe(201);
+    });
+  });
+
+  describe('POST /register/operator', () => {
+    beforeAll(async () => {
+      res = await request(server).post('/api/auth/register/operator').send({
+        username: 'operator3',
+        password: 'password',
+        email: 'operator@gmail.com'
+      });
+    });
+
+    it('should return a JSON object', () => {
+      expect(res.type).toBe('application/json');
+    });
+
+    it('should return a 201', () => {
+      expect(res.status).toBe(201);
+    });
+  });
+
+  describe('POST /login', () => {
+    beforeAll(async () => {
+      res = await request(server).post('/api/auth/login').send({
+        username: 'operator3',
+        password: 'password'
+      });
+    });
+
+    it('should return a JSON object', () => {
+      expect(res.type).toBe('application/json');
     });
   });
 });
